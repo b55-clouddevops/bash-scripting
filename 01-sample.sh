6c5688db031980d5c7bc2ff8018d3be885bb18e3
CodingManoj <mvp2612@hotmail.com>
2023-07-31 22:16:01 -0400

    Shebang Notation

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,1 +1,9 @@
-Sample file
+#!/bin/bash 
+
+# Shebang Notation : This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+echo Welcome To Bach Scripting 
+
+# This is how we can execute the scripts
+# $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
