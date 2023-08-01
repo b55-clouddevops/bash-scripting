8dc4820d2b43a0c199fe635774f19ae20b784a19
CodingManoj <mvp2612@hotmail.com>
2023-07-31 22:36:52 -0400

    AutoPush

---
+++ 02-escapeSequence.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# Shebang Notation : This tells the script with interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+echo Welcome To Bach Scripting 
+
+# This is how we can execute the scripts
+# $ bash scriptName.sh  or  sh scriptName.sh  or  ./scriptName.sh
+
+
+#  \n : This indicates a new line 
+#  Manoj\nDevOps
+#  Manoj
+#  DevOps
+
+echo Line1
+echo Line2
+echo Line3
+
+echo Line1\nLine2
