3decfad2003b23e6f7063417a5ae663b475b7341
CodingManoj <mvp2612@hotmail.com>
2023-08-01 22:30:09 -0400

    AutoPush

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -5,9 +5,10 @@
 
 # Here are few of the special variabes :  $0 to $9 , $? ,  $#,  $* , $@ 
 
-ROCKET_NAME=Chandrayaan
+# ROCKET_NAME=Chandrayaan
 echo $0                                 # $0 prints the script name you're executing 
 echo "Executed Script Name Is : $0"
 
+echo "Name of Recently Launched Rocket In India Is $1"
 
-echo "Name of Recently Launched Rocket In India Is $ROCKET_NAME"
+#  bash scriptName.sh  arg1  arg2  arg3
