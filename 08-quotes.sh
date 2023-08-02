6812d12d071420e1f833214e96370b11f37cfa57
CodingManoj <mvp2612@hotmail.com>
2023-08-01 22:59:26 -0400

    AutoPush

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -5,3 +5,6 @@ echo '$#'    # $# is going to print the number of arguments
 echo '$?'    # $? is going to print the exit code of the last command.
 echo '$*'    # $* is going to print the used arguments
 echo '$@'   # $* is going to print the used arguments
+
+# Single Quotes will suppress the power of a special variable
+# Whenever you have any special variables, prefer to avoid single quotes and go with double quotes
