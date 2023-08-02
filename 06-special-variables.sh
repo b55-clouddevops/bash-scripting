dea5e3a12c3a165ed306cd345e86966bc787abe6
CodingManoj <mvp2612@hotmail.com>
2023-08-01 22:41:29 -0400

    AutoPush

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -17,9 +17,9 @@ echo value of c is $c
 echo $0                                                # $0 prints the script name you're executing 
 echo "Executed Script Name Is : $0"
 
-echo "Name of Recently Launched Rocket In India Is $1"
-echo "Popular EV Vehicle In States Is $2"
-echo "Current Topic Is $3"
+# echo "Name of Recently Launched Rocket In India Is $1"
+# echo "Popular EV Vehicle In States Is $2"
+# echo "Current Topic Is $3"
 
 #  bash scriptName.sh  arg1  arg2  arg3
 # bash arg1  arg2  arg3 arg4  arg5  arg6 arg7  arg8  arg9  arg10 
