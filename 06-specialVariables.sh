e38c9da601b7e8d2d5fd48ccb7d827c2dc553f98
CodingManoj <mvp2612@hotmail.com>
2023-08-01 22:26:20 -0400

    AutoPush

---
+++ 06-specialVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+
+# Special variables gives special properties to your variables 
+
+# Here are few of the special variabes :  $0 to $9 , $? ,  $#,  $* , $@ 
+
+
+echo "Executed Script Name Is : 06-specialVariables.sh"
