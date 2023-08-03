540716e530965f2f1ac2d908efaafea9414743f9
CodingManoj <mvp2612@hotmail.com>
2023-08-02 23:08:22 -0400

    AutoPush

--- 15-ifelse-conditon.sh
+++ 15-ifelse-conditon.sh
@@ -1,13 +1,23 @@
 #/bin/bash 
 
-echo "Demo on if usage"
+echo "Demo on if and if-else and else-if usage"
 
 ACTION=$1 
 
-if [ "$ACTION" == " start" ]; then   
-    echo "Starting Payment"
+if [ "$ACTION" == "start" ]; then   
+    echo -e "\e[32m Starting Payment \e[0m"
     exit  0
-else 
-    echo "Valid Option is Start Only"
+
+else if [ "$ACTION" == "stop" ]; then 
+    echo -e "\e[31m Stopping Payment \e[0m"
     exit 1
+
+else if [ "$ACTION" == "restart" ]; then 
+    echo -e "\e[35m Restarting Payment \e[0m"
+    exit 2
+
+else 
+    echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+    echo -e "\e[33m Example Usage \e[0m :\n \t bash scriptName stop"
+    exit 3
 fi 
