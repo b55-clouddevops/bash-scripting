6bedbcb14221824415ffd9294e527ed56d593539
CodingManoj <mvp2612@hotmail.com>
2023-08-03 22:35:47 -0400

    Git Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,7 +5,7 @@
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ] ; then    
-    echo -e "\e[31m Script is expected to executed by the roor user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
     exit 1
 fi 
 
@@ -14,6 +14,12 @@ echo -e "\e[35m Configuring frontend......! \e[0m \n"
 echo "Installing Frontend :"
 yum install nginx -y   &>>  /tmp/frontend.log 
 
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m success \e[0m"
+else 
+    echo -e "\e[32m failure \e[0m"
+
+fi 
 
 # yum install nginx -y
 # systemctl enable nginx
