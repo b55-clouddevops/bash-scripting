0dac2feba9220e7aa2e85d9807326a0689b68bb5
CodingManoj <mvp2612@hotmail.com>
2023-08-03 22:39:18 -0400

    Git Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,10 +18,18 @@ if [ $? -eq 0 ]; then
     echo -e "\e[32m success \e[0m"
 else 
     echo -e "\e[31m failure \e[0m"
-
 fi 
 
-# yum install nginx -y
+echo -n "Starting Nginx:" 
+systemctl enable nginx
+systemctl start nginx
+
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32m success \e[0m"
+else 
+    echo -e "\e[31m failure \e[0m"
+fi 
+
 # systemctl enable nginx
 # systemctl start nginx
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
