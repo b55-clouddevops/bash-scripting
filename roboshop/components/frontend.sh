e21cb584b2f74f6749d582331220b32754f45a22
CodingManoj <mvp2612@hotmail.com>
2023-08-06 20:13:56 -0400

    Git Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -41,7 +41,7 @@ rm -rf *     &>>  /tmp/frontend.log
 stat $?
 
 echo -n "Extracting Frontend :"
-unzip -o /tmp/frontend.zip     &>>  /tmp/frontend.log
+unzip /tmp/frontend.zip     &>>  /tmp/frontend.log
 mv frontend-main/*  .
 mv static/* . 
 rm -rf frontend-main README.md
