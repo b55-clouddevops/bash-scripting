95d9dbb4bdea79f332962b04c6a29d4192a3afad
CodingManoj <mvp2612@hotmail.com>
2023-08-07 23:08:58 -0400

    Git Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -32,3 +32,7 @@ if [ $? -ne 0 ]; then
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
     stat $?
 fi 
+
+echo -n "Uninstalling Password-validate plugin :"
+echo "uninstall plugin validate_password" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
+stat $?
