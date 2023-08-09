87620a313b160fe11e793c11e272106d4033ecfe
CodingManoj <mvp2612@hotmail.com>
2023-08-08 22:21:58 -0400

    Git Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -49,8 +49,10 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Updating the Backend Components in the reverse proxy file:"
+
 for component in cataloue user cart ;do 
     sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done 
 
 echo -n "Restarting ${COMPONENT}:"
 systemctl daemon-reload     &>>  ${LOGFILE}
