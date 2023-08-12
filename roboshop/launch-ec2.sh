0dda048857b27a6f62082cd7ae716b460ba753c4
CodingManoj <mvp2612@hotmail.com>
2023-08-11 23:14:57 -0400

    Git Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -27,6 +27,8 @@ echo -e "Private IP Address of the $COMPONENT is $PRIVATEIP \n\n"
 echo -e "Creating DNS Record of ${COMPONENT}: "
 
 sed -e "s/COMPONENT/${COMPONENT}/"  -e "s/IPADDRESS/${IPADDRESS}/" route53.json  > /tmp/r53.json 
+cat /tmp/r53.json 
+
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 cat  /tmp/r53.json
 echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
